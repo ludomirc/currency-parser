81f9216fef77918b3217f1e63a3918ee32f9700a
Benek <beniamin.czaplicki@gmail.com>
2017-04-09 16:37:07 +0200

    update pom Add Money dependency

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -5,6 +5,7 @@ package pl.parser.nbp;
  */
 public class MainClass {
     public static void main(String[] args) {
+        // java.math.BigDecimal
 
     }
 }
---
+++ src/main/java/pl/parser/nbp/model/Pozycja.java
@@ -0,0 +1,14 @@
+package pl.parser.nbp.model;
+
+/**
+ * Created by Benek on 09.04.2017.
+ */
+public class Pozycja {
+
+    private String NazwaWaluty;
+    private Double Przelicznik;
+    private String KodWaluty;
+    private Double KursKupna;
+    private Double KursSprzedazy;
+
+}
