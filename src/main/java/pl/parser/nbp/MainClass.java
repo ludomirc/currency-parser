3c425ed9ee17388def9bd673f6052cf1f90c41ec
Benek <beniamin.czaplicki@gmail.com>
2017-04-09 17:16:14 +0200

    delete unused import

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -1,11 +1,31 @@
 package pl.parser.nbp;
 
+
+import pl.parser.nbp.model.TabelaKursow;
+
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
 /**
  * Created by Benek on 09.04.2017.
  */
 public class MainClass {
     public static void main(String[] args) {
-        // java.math.BigDecimal
+        try {
+
+            File file = new File("C:\\file.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(TabelaKursow.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            TabelaKursow tabelaKursow = (TabelaKursow) jaxbUnmarshaller.unmarshal(file);
+            System.out.println(tabelaKursow);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
 
     }
 }
