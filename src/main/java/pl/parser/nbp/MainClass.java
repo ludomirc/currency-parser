e645a763f92ef19c9cf0cf142932e7421fb8d4e2
Benek <beniamin.czaplicki@gmail.com>
2017-04-14 11:38:53 +0200

    improve logging

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -1,20 +1,30 @@
 package pl.parser.nbp;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import pl.parser.nbp.exception.AppException;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * Created by Benek on 09.04.2017.
  */
 public class MainClass {
 
-    private static Logger logger = LogManager.getLogger(MainClass.class.getName());
+    private static org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger(MainClass.class.getName());
 
     public static void main(String[] args) throws AppException {
+        disableDefaultLogger();
         logger.info("app start");
         AppContext.loadConfig();
         CurrencyCheck converter = new CurrencyCheck();
         converter.checkRate(args);
+
+    }
+
+    private static void disableDefaultLogger() {
+        LogManager.getLogManager().reset();
+        Logger globalLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        globalLogger.setLevel(java.util.logging.Level.OFF);
     }
 }
